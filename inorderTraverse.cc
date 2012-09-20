eac96df26b2fca448a852583ed1d398db7408b11
Xiaomou Wang <xiaomou@ubuntu.ubuntu-domain>
2012-09-20 02:14:24 -0400

    first commit

---
+++ inorderTraverse.cc
@@ -0,0 +1,35 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode *root) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        vector<int> res;
+        if (!root) { 
+            return res;
+        }
+        
+        if (root->left) {
+            res = inorderTraversal(root->left);
+        }
+        
+        res.push_back(root->val);
+        
+        if (root->right) {
+            vector<int> tmp = inorderTraversal(root->right);
+            res.insert( res.end(), tmp.begin(), tmp.end() );
+        }
+        
+        return res;
+                                    
+        
+    }
+};
