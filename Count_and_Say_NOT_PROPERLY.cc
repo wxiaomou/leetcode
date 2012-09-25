3ff1990f50890619453e8055046a7491f0ca68e1
wxiaomou <wxm19890422@gmail.com>
2012-09-24 23:23:52 -0400

    count and say

---
+++ Count_and_Say_NOT_PROPERLY.cc
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+using namespace std;
+class Solution {
+public:
+    string countAndSay(int n) {
+			if (n == 0) {
+				string ret1 ("");
+				return ret1;
+			}
+   		string ret ("1");
+			string res = ret;
+
+			// do it n times
+			for (int i = 0; i < n - 1 ; ++i) { 
+				int count = 1;
+				ret = res;
+				res.clear();// don't forget to clear res!!!!!!!!!!!!!!!
+				char tmp = ret[0];
+				if (i == 0) {
+					res = "11";
+				} else {
+				for (size_t j = 1; j < ret.length(); ++j) {
+					if (ret[j] == tmp) {
+						count++;
+						continue;
+					} else {
+						tmp = ret[j];
+						stringstream begin;
+						begin << count;
+						res += begin.str() + tmp;
+						count = 1;
+					}
+				}
+				// Don't forgot the last one
+				stringstream begin;
+				begin << count;
+				res += begin.str() + tmp;
+				cout << "res " << res << endl;
+			}
+			}
+
+			return res;
+		}
+};
+
+int main(int argc, char** argv) {
+	Solution sol;
+	cout << sol.countAndSay(3)	<< endl;
+	//string test ("1");
+	//cout << test.at(0);
+	return 1;
+}
