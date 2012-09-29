daff73760b7a63276747a94deb5c59ed58e3d356
wxiaomou <wxm19890422@gmail.com>
2012-09-28 21:59:32 -0400

    generateParenthess

---
+++ generateParenthess.cc
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+class Solution {
+public: 
+	vector<string> generateParenthesis(int n) {
+		vector<string> ret;
+		if (n <= 0)
+			return ret;
+		return recursion(n, n);
+	}	
+
+	vector<string> recursion(int left, int right) {
+		//if (left == 0 && right == 0)
+			//return ret;
+		vector<string> ret;
+		if (right == 0 && left ==0) {
+			string tmp("");
+			ret.push_back(tmp);
+			return ret;
+		}
+		vector<string> tmp;
+		if (right > 0) {
+			tmp = recursion(left, right - 1);
+				for (int i = 0; i < tmp.size(); ++i) {
+					string tmp1(")");
+					tmp[i] = tmp[i] + tmp1;
+					ret.push_back(tmp[i]);
+				}
+		} 
+		if (left > right) {
+				tmp = recursion(left - 1, right);
+				for (int i = 0; i < tmp.size(); ++i) {
+					string tmp1("(");
+					tmp[i] = tmp[i] + tmp1;
+					ret.push_back(tmp[i]);
+				}
+		} 
+		
+		return ret;
+		}
+	
+};
+
+int main(int argc, char** argv) {
+	Solution sol;
+	vector<string> tmp = sol.generateParenthesis(3);
+
+	for (int i = 0; i < tmp.size(); ++i) {
+		cout << tmp[i] << endl;
+	}
+	return 1;
+}
