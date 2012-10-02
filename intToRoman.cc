02db7737a398a1f16bfd4b1f37593b1be2d31bec
wxiaomou <wxm19890422@gmail.com>
2012-10-01 22:45:16 -0400

    int to roman

---
+++ intToRoman.cc
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+    class Solution {
+    public:
+        string intToRoman(int num) {
+            string ret("");
+    		if (!num)
+				return ret;
+			int base = 10;
+			while ((num % base) == 0) base *= 10;
+			// for the number only the first digit is not 0
+			if (base > num) {
+				string one;
+				string ten;
+				string five;
+
+				if (base == 10) {
+					one = 'I';
+					five = 'V';
+					ten = 'X';
+				} else if (base == 100) {
+					one = 'X';
+					five = 'L';
+					ten = 'C';
+				} else if (base == 1000) {
+					one = 'C';
+					five = 'D';
+					ten = 'M';
+				} else {
+					one = 'M'; // because input < 3999
+				}
+
+				int first = (num / (base / 10) )% 10;
+				switch(first) {
+				case 1:
+					return one;
+				case 2: 
+					return one + one;
+				case 3:
+					return one + one + one;
+				case 4:
+					return one + five;
+				case 5:
+					return five;
+				case 6:
+					return five + one;
+				case 7:
+					return five + one + one;
+				case 8: 
+					return five + one + one + one;
+				case 9:
+					return one + ten;
+				}
+			} else {
+				int cur = num % base;
+				int pre = num - cur;
+				return intToRoman(pre) + intToRoman(cur);
+			}
+        }
+    };
