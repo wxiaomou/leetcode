6d7b8faca1f33b7fd75e16b903baa655d4ad2eaf
wxiaomou <wxm19890422@gmail.com>
2012-10-21 16:47:42 -0400

    HARD

---
+++ minWindow.cc
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+#include <stddef.h>
+#include <limits.h>
+using namespace std;
+
+class Solution {
+public:
+	string minWindow(string S, string T) {
+		        int need[256];
+    	int have[256] = {0};
+
+		int begin = 0;
+		int end = 0;
+
+		for (int i = 0; i < 256; ++i) need[i] = -1;
+		for (int i = 0; i < T.size(); ++i) {
+			if (need[T[i]] == -1)
+				need[T[i]] = 1;
+			else
+				need[T[i]] ++;
+		}
+
+		int count = 0;
+		int length = INT_MAX;
+		int start = 0;
+
+		while (end < S.size()) {
+			if (need[S[end]] == -1) {
+				end++;
+				continue;
+			}
+			have[S[end]]++;
+			if (have[S[end]] <= need[S[end]])
+				count++;
+			if (count == T.length()) {
+				while (need[S[begin]] == -1 || have[S[begin]] > need[S[begin]]) {
+					if (have[S[begin]] > need[S[begin]])
+						have[S[begin]]--;
+					begin++;
+				}
+				if (end - begin + 1 < length) {
+					start = begin;
+					length = end - begin + 1;
+				}
+				
+			}
+        end++;		
+    	
+		}
+
+		return length <= S.length() ? S.substr(start, length) : "";
+	}
+
+};
+
+int main (int argc, char **argv) {
+	Solution sol;
+	cout << sol.minWindow("bdab", "ab") << endl;
+	return 0;
+}
