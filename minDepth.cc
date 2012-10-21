f25de9450ff12bab4156ed95b85030d15d787261
wxiaomou <wxm19890422@gmail.com>
2012-10-21 10:59:51 -0400

    what is a leaf node

---
+++ minDepth.cc
@@ -0,0 +1,32 @@
+/**
+ * Definition for binary tree
+ */
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+class Solution {
+public:
+    int minDepth(TreeNode *root) {
+					if (!root)
+				return 0;
+
+			int left = minDepth(root->left);
+			int right = minDepth(root->right);
+			
+			if (left ^ right)
+				return min(left, right) + 1;
+			else 
+				return max(left, right) + 1;
+    }
+};
+
+int main (int argc, char **argv) {
+	return 0;
+}
