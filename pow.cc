f194433489e27977fbb3cdc94ab61f5ed4f145ab
wxiaomou <wxm19890422@gmail.com>
2012-10-23 18:54:24 -0400

    negtive situation

---
+++ pow.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+	double pow(double x, int n) {
+		      if (!n)
+    		return 1.0;
+		 
+		double ret = 1.0;
+		bool neg = false;
+		if (n < 0) {
+			n = -n;
+			neg = true;
+		}
+		if (n % 2) {
+			double tmp = pow(x, n >> 1);
+			ret =  x * tmp * tmp;
+		} else {
+			double tmp = pow(x, n >> 1);
+			ret = tmp * tmp;
+		}
+
+		if (neg) 
+			return 1.0 / ret;
+		else 
+			return ret;
+	}
+};
