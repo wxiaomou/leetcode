b31ad330fca6b629381d6226f44e5fcb2438b7c4
wxiaomou <wxm19890422@gmail.com>
2012-10-29 15:16:21 -0400

    HARD

---
+++ searchMatrix.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	bool searchMatrix(vector<vector<int> > &matrix, int target) {
+		int i = 0;
+    	int j = matrix[0].size() - 1;
+
+		while (j >= 0 && i < matrix.size()) {
+			if (matrix[i][j] > target)
+				j--;
+			else if(matrix[i][j] < target)
+				i++;
+			else 
+				return true;
+		}
+		return false;
+	}
+};
+
+int main(int argc, char **argv) {
+	return 0;
+}
