6614b589b014692688fefefbdb7bd5daab2c7b8b
wxiaomou <wxm19890422@gmail.com>
2012-11-02 10:47:07 -0400

    MID

---
+++ uniqueBST2.cc
@@ -0,0 +1,50 @@
+/**
+ * Definition for binary tree
+ */
+#include <iostream>
+#include <vector>
+#include <stddef.h>
+
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+class Solution {
+public:
+		vector<TreeNode *> generateTrees(int n) {
+			return doGenerateTrees(1, n);
+		}
+
+    vector<TreeNode *> doGenerateTrees(int start, int end) {
+			vector<TreeNode *> res;
+			if(start > end) {
+				res.push_back(NULL);
+				return res;
+			}
+			for (int i = start; i <= end; i++) {
+				vector<TreeNode *> left1 = doGenerateTrees(start, i - 1);
+				vector<TreeNode *> right1 = doGenerateTrees(i + 1, end);
+				for (int j = 0; j < left1.size(); j++) {
+					for (int k = 0; k < right1.size(); k++) {
+						TreeNode *root = new TreeNode(i);
+						root->left = left1[j];
+						root->right = right1[k];
+						
+						res.push_back(root);
+					}
+				}
+			}	
+			return res;
+    }
+
+		
+};
+
+int main(int argc, char **argv) {
+	Solution sol;
+	sol.generateTrees(1);
+	return 0;
+}
