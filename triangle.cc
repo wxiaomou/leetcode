3b3412b5dfb530d620e2bbe2a0152cfb59050e11
Xiaomou Wang <xiaomouwang@Xiaomous-MacBook-Pro.local>
2013-11-09 10:09:22 -0800

    use temp array

--- triangle.cc
+++ triangle.cc
@@ -1,25 +1,16 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
-using namespace std;
 class Solution {
-public:
-	int minimumTotal(vector<vector<int> > &triangle) {
-		if (triangle.size() == 0)
-			return 0;
-
-		int n = triangle.size() - 2;
-		while (n >= 0) {
-			for (int i = 0; i <= n; i++) {
-				triangle[n][i] += min(triangle[n + 1][i], triangle[n + 1][i + 1]);
-			}
-			n--;
-		}
-		return triangle[0][0];
-	}
+    public:
+    int minimumTotal(vector<vector<int> > &triangle) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        vector<int> ref = triangle[triangle.size() - 1];
+        if (triangle.size() == 0) return 0;
+        for (int i = triangle.size() - 2; i >= 0; --i) {
+            for (int j = 0; j < triangle[i].size(); j++) {
+                ref[j] = triangle[i][j] + min(ref[j], ref[j + 1]);
+            }
+        }
+        return ref[0];
+    }
+    
 };
-
-int main(int argc, char **argv) {
-	return 0;
-}
