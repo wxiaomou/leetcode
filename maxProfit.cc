70079b155ef72a00e7ea0e206736570bc6032333
Xiaomou Wang <xiaomouwang@Xiaomous-MacBook-Pro.local>
2013-11-10 20:13:48 -0800

    mid

---
+++ maxProfit.cc
@@ -0,0 +1,19 @@
+class Solution {
+    public:
+    int maxProfit(vector<int> &prices) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        if (!prices.size()) return 0;
+        int _min = prices[0];
+        int i = 0;
+        int ret = 0;
+        
+        while (i < prices.size()) {
+            _min = min(_min, prices[i]);
+            ret = max(ret, prices[i] - _min);
+            i++;
+        }
+        return ret;
+        
+    }
+};
