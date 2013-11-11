44c4131014f402419c1d4fbe85a8816797cc307f
Xiaomou Wang <xiaomouwang@Xiaomous-MacBook-Pro.local>
2013-11-10 23:11:10 -0800

    iteratively way

---
+++ preorderTraversal.cc
@@ -0,0 +1,28 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+    public:
+    vector<int> preorderTraversal(TreeNode *root) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        vector<int> ret;
+        if (!root) return ret;
+        stack<TreeNode *> s;
+        s.push(root);
+        while (s.size()) {
+            TreeNode *tmp = s.top();
+            ret.push_back(tmp->val);
+            s.pop();
+            if (tmp->right) s.push(tmp->right);
+            if (tmp->left) s.push(tmp->left);
+        }
+        return ret;
+    }
+};
