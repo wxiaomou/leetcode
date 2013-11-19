9ce0204895827c92b39a64975787d85b350e9a8e
Xiaomou Wang <xiaomouwang@Xiaomous-MacBook-Pro.local>
2013-11-19 12:29:43 -0800

    ease mid

---
+++ copyRandomList.cc
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list with a random pointer.
+ * struct RandomListNode {
+ *     int label;
+ *     RandomListNode *next, *random;
+ *     RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    map<RandomListNode *, RandomListNode *> hash;
+    RandomListNode *copyRandomList(RandomListNode *head) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        if (!head) return NULL;
+        if (hash.find(head) != hash.end())
+            return hash.find(head)->second;
+        RandomListNode *ret = new RandomListNode(head->label);
+        hash.insert(pair<RandomListNode *, RandomListNode *>(head, ret));
+        if (head->next) 
+            ret->next = copyRandomList(head->next);
+        if (head->random)
+            ret->random = copyRandomList(head->random);
+        return ret;
+    }
+};
