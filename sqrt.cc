adb994f4c0adea5b6840add8e34b9cc20f99dbe8
Xiaomou Wang <wxm19890422@gmail.com>
2013-11-29 16:30:43 -0800

    mid hard

--- sqrt.cc
+++ sqrt.cc
@@ -15,14 +15,14 @@ public:
 		while (low < up) {
 			mid = (up + low) / 2;
 			if (mid > 46340) {
-				up = mid;
+				up = 46341; // cannot use 46340 cause we will not try high(becuase low < hign) so use 46340 will lost itself.
 				continue;
 			}
 			if (mid * mid == x)
 				return mid;
 			if (mid * mid > x) {
 				up = mid;
-			} else { 
+			} else { // if the number cannot not sqrt by a int, we need to return the larger one which square less than that, eg. if x = 14 return 3, so we only update last here. 
 				low = mid + 1;
 				last = mid;
 			}
