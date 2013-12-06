a5564613b8181a4941350c5b9e77cc9fb2712471
Xiaomou Wang <wxm19890422@gmail.com>
2013-12-05 23:42:56 -0800

    mid hard O(1) space

--- setMatrixZeros.cc
+++ setMatrixZeros.cc
@@ -52,6 +52,32 @@ public:
 
 		return;
 	}
+
+	/* O(m + n) space
+	void setZeroes(vector<vector<int> > &matrix) {
+        int m = matrix.size();
+        if (!m) return;
+        int n = matrix[0].size();
+        unordered_set<int> row;
+        unordered_set<int> col;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    row.insert(i);
+                    col.insert(j);
+                }
+            }
+        }
+        
+        for ( auto i = row.begin(); i != row.end(); i++) {
+            for ( auto j = 0; j < n; ++j) matrix[*i][j] = 0;
+        }
+        
+        for ( auto j = col.begin(); j != col.end(); ++j) {
+            for ( auto i = 0; i < m; i++) matrix[i][*j] = 0;
+        }   
+    }
+	*/
 };
 
 int main(int argc, char **argv) {
