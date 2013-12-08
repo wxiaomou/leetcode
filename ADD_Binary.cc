b6a17bde165af87be86ad4f358986abb9a9bbece
Xiaomou Wang <wxm19890422@gmail.com>
2013-12-08 14:25:56 -0800

    mid

--- ADD_Binary.cc
+++ ADD_Binary.cc
@@ -3,7 +3,49 @@
 using namespace std;
 class Solution {
 public:
-    string addBinary(string a, string b) {
+	string addBinary(string a, string b) {
+        auto m = a.length();
+        auto n = b.length();
+        if (!m || !n) return m ? a : b;
+        int adv = 0;
+        int i = m - 1;
+        int j = n - 1;
+        string c = ""; 
+        int a1 = (a[i] - '0');
+        int b1 = (b[j] - '0');
+        while (i >= 0 || j >= 0) {
+            int tmp = a1 + b1 + adv;
+            if (tmp >= 2) {
+                tmp %= 2;
+                adv = 1;
+            } else {
+                adv = 0;
+            }
+            string tmp1 = to_string(tmp);
+            c = tmp1 + c;
+            i--;
+            j--;
+            
+            if (i >= 0) {
+                a1 = a[i] - '0';
+            } else {
+                a1 = 0;
+            }
+            
+            if (j >= 0) {
+                b1 = b[j] - '0';  
+            } else {
+                b1 = 0;
+            }
+        }
+        
+        if (adv) {
+            string tmp = "1";
+            c = tmp + c;
+        }
+        return c;
+    }
+    /*string addBinary(string a, string b) {
     	int i = a.length() - 1;
 			int j = b.length() - 1;
 			int k = i > j ? i : j;
@@ -48,7 +90,7 @@ public:
 			if (carry)
 				ret = '1' + ret;
 			return ret;
-		}
+		}*/
 };
 
 
