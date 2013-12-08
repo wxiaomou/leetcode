d29cac6117d63d03649ec916317e29c25e5ea215
Xiaomou Wang <wxm19890422@gmail.com>
2013-12-08 14:03:18 -0800

    Very Hard

--- largestRectArea.cc
+++ largestRectArea.cc
@@ -14,7 +14,7 @@ class Solution {
        		int area[n];
        		for (int i = 0; i < n; ++i) area[i] = 0; // count the longest width for each height
        		for (int i = 0; i < n; ++i) {
-           		int t = -1;
+           		int t = -1; // !!!t is the rightest index which ouosidet the range in which height[i] is minimum, so if s is empty t should be -1, because 0 is valid
            		while (!s.empty() && height[s.top()] >= height[i]) s.pop(); //find all the height continus on the left, !!!attention need to be >= here
            		if (!s.empty())
                		t = s.top(); // if empty the all prevois larger than current height
@@ -23,7 +23,7 @@ class Solution {
        		}
        		while (!s.empty()) s.pop();
        		for (int i = n - 1; i >= 0; --i) {
-           		int t = n;
+           		int t = n; // !!!t is the leftest item which ouosidet the range in which height[i] is minimum, so t = s.top() and we don't need to + 1, becuse t is leftest outside range
            		while (!s.empty() && height[s.top()] >= height[i]) s.pop(); //find all the height continus on the right
            		if (!s.empty())
                		t = s.top();
