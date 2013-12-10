0e14c25b87171a3933e505db4d10636432ba30e2
Xiaomou Wang <wxm19890422@gmail.com>
2013-12-09 17:06:07 -0800

    ease

--- singleNumber.cc
+++ singleNumber.cc
@@ -1,8 +1,6 @@
 class Solution {
 public:
     int singleNumber(int A[], int n) {
-        // IMPORTANT: Please reset any member data you declared, as
-        // the same Solution instance will be reused for each test case.
         if (!n) return 0;
         int ret = 0;
         for (int i = 0; i < n; i++) {
