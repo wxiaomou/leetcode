a785f14c2c57d578edec6fca7f277dbebaee0a61
Xiaomou Wang <wxm19890422@gmail.com>
2013-12-10 10:16:09 -0800

    mid

--- uniquePaths2.cc
+++ uniquePaths2.cc
@@ -36,6 +36,39 @@ public:
 		}
 		return dp[0][0];
 	}
+	/*
+	 int uniquePathsWithObstacles(vector<vector<int> > &obstacleGrid) {
+        int m = obstacleGrid.size();
+        if (!m) return 0;
+        int n = obstacleGrid[0].size();
+        int matrix[m][n];
+        if (obstacleGrid[m - 1][n - 1]) return 0;
+        matrix[m - 1][n - 1] = 1;
+        for ( auto i = m - 2; i >= 0; --i ) {
+            if (obstacleGrid[i][n - 1] != 1 && matrix[i + 1][n - 1] == 1)
+                matrix[i][n - 1] = 1;
+            else
+                matrix[i][n - 1] = 0;
+        }
+        for ( auto j = n - 2; j >= 0; --j ) {
+            if (obstacleGrid[m - 1][j] != 1 && matrix[m - 1][j + 1] == 1)
+                matrix[m - 1][j] = 1;
+            else
+                matrix[m - 1][j] = 0;
+        }
+        
+        for ( auto i = m - 2; i >= 0; --i ) {
+            for ( auto j = n - 2; j >= 0; --j ) {
+                if ( obstacleGrid[i][j] == 1) {
+                    matrix[i][j] = 0;
+                    continue;
+                }
+                matrix[i][j] = matrix[i + 1][j] + matrix[i][j + 1];
+            }
+        }
+        return matrix[0][0];
+    }
+	*/
 };
 
 int main(int argc, char **argv) {
