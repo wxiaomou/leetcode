53b5cea4e7bfb4cadbcd3fae52f7847a750882b3
Xiaomou Wang <wxm19890422@gmail.com>
2013-12-13 10:46:05 -0800

    mid hard

--- lengthOfLongestSubstring.cc
+++ lengthOfLongestSubstring.cc
@@ -5,7 +5,7 @@ using namespace std;
 
 class Solution {
 public:
-	int lengthOfLongestSubstring(string s) {
+	i/*nt lengthOfLongestSubstring(string s) {
     if (!s.length())
 			return 0;
 
@@ -32,30 +32,35 @@ public:
 	
 		}
 		return max;
-	}
+	}*/
 
-	/* above is better, nearly O(n)
+	
 	int lengthOfLongestSubstring(string s) {
         unordered_map<char, int> hash;
+        hash.clear();
         int ret = 0;
         int tmp = 0;
-        int start = 0;
+        int j = 0;
         for ( auto i = 0; i < s.length(); ++i ) {
-            if (hash.count(s[i]) > 0) {
-                ret = max(tmp, ret);
-                start = hash[s[i]] + 1;
-                i = hash[s[i]];
-                hash.clear();
-                tmp = 0;
+             if (hash.count(s[i]) > 0) {
+                int index = hash[s[i]];
+                while (j < index) {
+                    hash.erase(s[j]);                  
+                }
+                j++;
+                hash[s[i]] = i;// need to insert s[i] here, O.W. we will miss this one, just change the value to be the new i of key s[i],
+                			  // don't forget increase j, j should always be the start index of current sequence!!!
+                tmp = i - index;
             } else {
                 hash[s[i]] = i;
                 tmp++;
+                ret = max(tmp, ret);
             }
         }
-        ret =  ret = max(tmp, ret);
         return ret;
     }
-	*/
+
+	
 };
 
 int main(int argc, char **argv) {
