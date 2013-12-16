8ae17f4ea521e0f687736f7f0913db15c881286d
Xiaomou Wang <wxm19890422@gmail.com>
2013-12-16 10:11:05 -0800

    mid

--- zigZagConversion.cc
+++ zigZagConversion.cc
@@ -22,6 +22,30 @@ public:
 		}
 		return ret;
 	}
+	/*
+	string convert(string s, int nRows) {
+        if (nRows < 2) return s;
+        int n = s.length();
+        string ret = "";
+        for ( auto i = 0; i < nRows; ++i ) {
+            int j = i;
+            while (j < n) {
+                if (i == 0 || i == nRows - 1) {
+                    ret += s[j];
+                    j += nRows + nRows - 2;
+                } else {
+                    ret += s[j];
+                    j += (nRows - 1 - i) * 2;
+                    if (j < n) {
+                        ret += s[j];
+                        j += i * 2;
+                    }
+                }
+            }
+        }
+        return ret;
+    }
+	*/
 };
 
 int main(int argc, char **argv) {
