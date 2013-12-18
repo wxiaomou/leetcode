4bd98e73012cd26efe95f5690532f43d07250333
Xiaomou Wang <wxm19890422@gmail.com>
2013-12-18 13:17:39 -0800

    mid

--- longestCommonPrefix.cc
+++ longestCommonPrefix.cc
@@ -5,12 +5,36 @@ using namespace std;
 class Solution {
 public:
 	string longestCommonPrefix(vector<string> &strs) {
+        if (!strs.size()) return "";
+        int i = 0;
+        string ret = "";
+        while (true) {
+            if (i >= strs[0].size())
+                break;
+            char ref;
+            ref = strs[0].at(i);
+            int j;
+            for (j = 1; j < strs.size(); ++j) {
+                if (strs[j].size() <= i)
+                    break;
+                if (ref != strs[j][i])
+                    break;
+            }
+            if (j == strs.size())
+                ret += ref;
+            else
+                break;
+            i++;
+        }
+        return ret;
+    }
+	/*string longestCommonPrefix(vector<string> &strs) {
 		string ret = "";
 		if (!strs.size())
 			return ret;
 		int i = 0;
 		while(true) {
-			string tmp;
+			string tmp; //string tmp = strs[0].at(i); won't work
 			if (strs[0].size() <= i)
 				break;
 			else
@@ -29,7 +53,7 @@ public:
 			i++;
 		}
 		return ret;
-	}
+	}*/
 };
 
 int main(int argc, char **argv) {
