dae802529c2b52cc65fccbfe56d898ba0757bd2a
Xiaomou Wang <wxm19890422@gmail.com>
2013-12-20 14:29:59 -0800

    mid

--- reorderList.cc
+++ reorderList.cc
@@ -8,9 +8,45 @@
  */
 class Solution {
 public:
-    void reorderList(ListNode *head) {
+    //smarter way to handle the odd or even issue.
+     void reorderList(ListNode *head) {
+        if (!head || !head->next) return; 
+        ListNode *pre = head;
+        ListNode *post = head;
+        stack<ListNode *> s;
+        while (post) {
+            if (post->next && post->next->next) {
+                post = post->next->next;
+                pre = pre->next;
+            } else {
+                break;
+            }
+        }
+        post = pre->next;
+        while(post) {
+            s.push(post);
+            post = post->next;
+        }
+        pre = head;
+        while (s.size()) {
+            ListNode *tmp = pre->next;
+            ListNode *tmp1 = s.top();
+            if (tmp == tmp1) { //if total nodes num is even and now pre is the last one, e.g 1->2->3->4, now tmp == tmp1 == 3, pre is 2
+                pre = pre->next;
+                break;
+            }
+            s.pop();
+            pre->next = tmp1;
+            pre = pre->next;
+            pre->next = tmp;
+            pre = pre->next;
+        }
+        pre->next = NULL;
+    }
+    /*void reorderList(ListNode *head) {
         // IMPORTANT: Please reset any member data you declared, as
         // the same Solution instance will be reused for each test case.
+
         if (!head || !head->next || !head->next->next) return;
         ListNode *first = head;
         ListNode *second = head;
@@ -47,5 +83,5 @@ public:
             } 
         }
         head = ret;        
-    }
+    }*/
 };
