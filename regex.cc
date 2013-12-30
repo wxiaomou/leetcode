21db1e88638ae0940c90807fe7495bf5c1d36140
Xiaomou Wang <wxm19890422@gmail.com>
2013-12-30 15:54:20 -0800

    Hard

--- regex.cc
+++ regex.cc
@@ -18,22 +18,20 @@ public:
     /*bool isMatch(const char *s, const char *p) {
         if (*p == 0) return *s == 0;
         if (*s == 0) {
-            if (*(p + 1) != '*')
-                return false;
-            else if (*p == '.')
-                    return isMatch(s, p + 2);
+            if (*(p + 1) == '*') return isMatch(s, p + 2);
+            return false;            
         }
-        if (*(p + 1) != '*') {
-            if (*s == *p || *p == '.')
-                return isMatch(s + 1, p + 1);
-            else 
-                return false;
-        } else {
-            while (*s == *p || (*p == '.' && *s != 0)) {
-                if (isMatch(s, p + 2)) return true;
+        
+        if (*(p + 1) == '*') {
+            while ((*s == *p || *p == '.') && *s != 0) { // if *s == *p than '*' will match 1 or more
+                if (isMatch(s, p + 2)) return true; 
                 s++;
             }
-            return isMatch(s, p + 2);
+            return isMatch(s, p + 2); //if *s != *p then x* match zero item.
+        } else {
+            if (*s == *p || *p == '.') return isMatch(s + 1, p + 1);
+            return false;
         }
+    }
     }*/
 };
