b4c39fb76e3578211adea82ce383207a184ff9cc
Xiaomou Wang <wxm19890422@gmail.com>
2014-01-01 11:04:00 -0800

    mid

--- isValidSudoku.cc
+++ isValidSudoku.cc
@@ -2,29 +2,27 @@ class Solution {
 public:
     bool isValidSudoku(vector<vector<char> > &board) {
         unordered_set<int> hashset;
-        int m = board.size();
-        int n = board[0].size();
         // for each line;
-        for ( auto i = 0; i < m; ++i ) {
+        for ( auto i = 0; i < 9; ++i ) {
             hashset.clear();
-            for (auto j = 0; j < n; ++j) {
+            for (auto j = 0; j < 9; ++j) {
                 if (board[i][j] != '.' && hashset.count(board[i][j]) > 0)
                     return false;
                 hashset.insert(board[i][j]);
             }
         }
         // for each col
-        for ( auto j = 0; j < n; ++j ) {
+        for ( auto j = 0; j < 9; ++j ) {
             hashset.clear();
-            for (auto i = 0; i < m; ++i ) {
+            for (auto i = 0; i < 9; ++i ) {
                 if (board[i][j] != '.' && hashset.count(board[i][j]) > 0)
                     return false;
                 hashset.insert(board[i][j]);
             }
         }
         // for each sub
-        for ( auto i = 0; i < m; i += 3 ) {
-            for ( auto j = 0; j < n; j += 3 ) {
+        for ( auto i = 0; i < 9; i += 3 ) {
+            for ( auto j = 0; j < 9; j += 3 ) {
                 hashset.clear();
                 for ( auto k1 = i; k1 < i + 3; k1++ ) {
                     for ( auto k2 = j; k2 < j + 3; k2++ ) {
