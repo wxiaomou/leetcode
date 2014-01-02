ac4e660cfb415d750524aad42d48a6ed44788535
Xiaomou Wang <wxm19890422@gmail.com>
2014-01-01 17:07:58 -0800

    mid hard

---
+++ combinationSum2.cc
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int> > combinationSum2(vector<int> &candidates, int target) {
+        int n = candidates.size();
+        if (!n) return vector<vector<int>>();
+        sort(candidates.begin(), candidates.end());
+        return comb(candidates, target, 0);
+    }
+
+    vector<vector<int>> comb(vector<int> &candidates, int target, int begin) {
+        int n = candidates.size();
+        vector<vector<int>> ret;
+        
+        for (int i = begin; i < n; ) {
+            if (candidates[i] == target) {
+                ret.push_back(vector<int>(1, target));
+                return ret;
+            }
+            if (candidates[i] < target) {
+                vector<vector<int>> tmp = comb(candidates, target - candidates[i], i + 1);
+                if (tmp.size()) {
+                    for (int j = 0; j < tmp.size(); j++) {
+                        tmp[j].insert(tmp[j].begin(), candidates[i]);
+                    }
+                    ret.insert(ret.end(), tmp.begin(), tmp.end());
+                }
+            }
+            int ref = candidates[i];
+            while (ref == candidates[i]) i++;
+            if (candidates[i] > target) break;
+        }
+        return ret;
+    }
+
+};
