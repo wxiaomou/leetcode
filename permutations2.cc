be4efaee85db3b24421a6542d6931eadb763b49d
Xiaomou Wang <wxm19890422@gmail.com>
2014-01-03 18:11:23 -0800

    mid

--- permutations2.cc
+++ permutations2.cc
@@ -4,6 +4,36 @@
 using namespace std;
 class Solution {
 public:
+	vector<vector<int> > permuteUnique(vector<int> &num) {
+        int n = num.size();
+        if (!n) return vector<vector<int>>();
+        vector<bool> ref(n, false);
+        sort(num.begin(), num.end());
+        return _permute(num, ref);
+    }
+    vector<vector<int> > _permute(vector<int> &num, vector<bool> &ref) {
+        vector<vector<int>> ret;
+        vector<vector<int>> tmp;
+        for ( auto i = 0; i < num.size(); ) {
+            int a = num[i];
+            if (!ref[i]) {
+                ref[i] = true;
+                tmp = _permute(num, ref);
+                for ( auto j = 0; j < tmp.size(); ++j) {
+                    tmp[j].insert(tmp[j].begin(), num[i]);
+                }
+                ret.insert(ret.end(), tmp.begin(), tmp.end());
+                ref[i] = false;
+                while(a == num[i]) i++;
+            } else {
+                i++;
+            }
+            
+        }
+        if (!tmp.size()) ret.push_back(vector<int>());
+    	return ret;
+    }
+	//--------------------------------------------//
 	vector<vector<int> > permute(vector<int> &num) {
 		vector<vector<int> > res;
 		if (!num.size())
