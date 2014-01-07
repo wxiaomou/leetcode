903a9cd627099e9dcafe2e71ce8b18e22fcf2b0f
Xiaomou Wang <wxm19890422@gmail.com>
2014-01-06 22:11:08 -0800

    mid hard

--- jumpGame_NB.cc
+++ jumpGame_NB.cc
@@ -17,6 +17,27 @@ public:
 		}
 
 		return m >=i n - 1;
+
+		//--------------------------//
+		bool canJump(int A[], int n) {
+        if(!n) return true;
+        int i = 0;
+        int dist = i + A[i];
+        while (i < n) {
+            int ref = i;
+            int j;
+            for (j = i; j < n && j <= dist; j++) {
+                if (ref + A[ref] <= j + A[j]) {
+                    ref = j;
+                }
+            }
+            if (j >= n) return true;
+            i = ref;
+            dist = i + A[i];
+            if (dist >= n - 1) return true;
+            if (dist == i) return false;
+        }
+    }
 	}
 
 
