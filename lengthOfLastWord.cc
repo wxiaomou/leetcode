24951179343908855eb45870755a1bcab736b9c2
Xiaomou Wang <wxm19890422@gmail.com>
2014-01-08 20:56:40 -0800

    mid

--- lengthOfLastWord.cc
+++ lengthOfLastWord.cc
@@ -4,8 +4,31 @@ using namespace std;
 
 class Solution {
 public:
+	 int lengthOfLastWord(const char *s) {
+        if (*s == '\0') return 0;
+        const char *str = s;
+        int len = 0;
+        bool space = false;
+        while (*str != '\0') {
+            if (space) {
+                len = 0; 
+                space = false;
+            }
+            while (*str != '\0' && *str != ' ') {
+                len++;
+                str++;
+            }
+            if (*str == '\0') break;
+            while (*str == ' ') {
+                space = true;
+                str++;
+            }
+        }
+        return len;
+    }
+	//-------------------------------------//
 	int lengthOfLastWord(const char *s) {
-            int len = 0;
+        int len = 0;
 		if (!s)
 			return len;
 		int i = 0;
