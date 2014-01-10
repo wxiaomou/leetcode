466003dca2588605ee20e8a0ab59dcac7705b6c4
Xiaomou Wang <wxm19890422@gmail.com>
2014-01-09 21:57:46 -0800

    mid hard

--- searchRotatedArray.cc
+++ searchRotatedArray.cc
@@ -3,6 +3,39 @@
 using namespace std;
 class Solution {
 public:
+
+
+// key: use A[mid] and A[start] to find the increase part, if target inside the increase part then choose accoeding part else choose the oposite
+ int search(int A[], int n, int target) {
+        if (!n) return -1;
+        int start = 0;
+        int end = n - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (A[mid] == target) return mid;
+            if (target == A[end]) return end;
+            if (target == A[start]) return start;
+            
+        
+            if (A[mid] < A[start]) {
+                if (target > A[mid] && target < A[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            } else {
+                if (target > A[start] && target < A[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        
+        return -1;
+    }
+
+  //---------------------------------------//
 	int search(int A[], int n, int target) {
 		int i = 0;
     	if (target < A[0]) {
