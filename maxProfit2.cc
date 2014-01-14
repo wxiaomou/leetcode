b1a661feace38a8a28573f18d4397e6f31c656c3
Xiaomou Wang <wxm19890422@gmail.com>
2014-01-13 20:17:12 -0800

    mid hard

--- maxProfit2.cc
+++ maxProfit2.cc
@@ -12,4 +12,24 @@ class Solution {
         }
         return ret;
     }
+
+    //--------------------------------------------------------//
+    int maxProfit(vector<int> &prices) {
+        int n = prices.size();
+        if (n < 2) return 0;
+        int i = 1;
+        int ret = 0;
+        while (i < n) {
+            int start = i - 1;
+            while (i < n && prices[i] > prices[i - 1]) {
+                i++;
+            }
+            if (i > start + 1) {
+                ret += prices[i - 1] - prices[start];
+            } else {
+                i++;
+            }
+        }
+        return ret;
+    }
 };
