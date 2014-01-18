af056b047ba65cb8819527e70ea522ddf64f3ff5
Xiaomou Wang <wxm19890422@gmail.com>
2014-01-17 22:45:41 -0800

    Hard

--- longestPalindrome.cc
+++ longestPalindrome.cc
@@ -6,6 +6,46 @@ using namespace std;
 class Solution {
 public:
 	string longestPalindrome(string s) {
+        int n = s.size();
+        if (!n) return "";
+        string ret = "";
+        int len = 0;
+        for (int i = 0; i < n; i++) {
+            string tmp;
+            int tmp_len = _length(i, s, tmp);
+            if (tmp_len > len)  {
+                ret = tmp;
+                len = tmp_len;
+            }
+        }
+        return ret;
+    }
+    
+    int _length(int i, const string s, string &tmp) {
+        int j = 1;
+        int len = 1;
+        while (i - j >= 0 && i + j < s.size()) {
+            if (s[i - j] != s[i + j]) break;
+            len += 2;
+            j++;
+        }
+        tmp = s.substr(i - j + 1, len);
+        j = i + 1;
+        int len1 = 0;
+        while (i >= 0 && j < s.size()) {
+            if (s[i] != s[j]) break;
+            len1 += 2;
+            j++;
+            i--;
+        }
+        if (len1 > len) {
+            tmp = s.substr(i + 1, len1);
+            return len1;
+        }
+        return len;
+    } 
+	//----------------------------------------//
+	string longestPalindrome(string s) {
             int n = s.length();
             if (!n) return s;
             string ret;return res1.length() > res2.length() ? res1 : res2;
