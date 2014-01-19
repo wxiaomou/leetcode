28954ef13d463b8efcfc3412aeea62fb8e69008f
Xiaomou Wang <wxm19890422@gmail.com>
2014-01-20 05:15:19 +0800

    mid Hard

--- evalRPN.cc
+++ evalRPN.cc
@@ -12,17 +12,47 @@ public:
                 int tmp = s.top();
                 s.pop();
                 if (c == "+")
-                        s.top() += tmp;
+                    s.top() += tmp;
                 else if (c == "-")
-                        s.top() -= tmp;
+                    s.top() -= tmp;
                 else if (c == "*")
-                        s.top() *= tmp;
+                    s.top() *= tmp;
                 else if (c == "/")
-                        s.top() /= tmp;
+                    s.top() /= tmp;
                 else
-                        return -1;
+                    return -1;
             }
         }
         return s.top();
+    }
+
+    //----------------------------------------------//
+    int evalRPN(vector<string> &tokens) {
+        int n = tokens.size();
+        if (n == 1) return (atoi(tokens[0].c_str()));
+        if (!n) return 0;
+        stack<int> s;
+        int ret = 0;
+        for (int i = 0; i < n; i++) {
+            if (tokens[i] != "+" && tokens[i] != "-" && tokens[i] != "*" && tokens[i] != "/") {
+                s.push(atoi(tokens[i].c_str()));
+            } else {
+                ret = s.top();
+                s.pop();
+                if (!s.size()) return ret;
+                if (tokens[i] == "+") {
+                    ret += s.top();
+                } else if (tokens[i] == "-") {
+                    ret = s.top() - ret;
+                } else if (tokens[i] == "*") {
+                    ret *= s.top();
+                } else {
+                    ret = s.top() / ret;
+                }
+                s.pop();
+                s.push(ret);
+            }
+        }
+        return ret;
     }
 };
