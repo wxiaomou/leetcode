faea571d9056f59e828b394a374eb052cb646454
Xiaomou Wang <wxm19890422@gmail.com>
2014-01-22 06:52:59 +0800

    ease

--- hasCycle.cc
+++ hasCycle.cc
@@ -8,6 +8,18 @@
  */
 class Solution {
 public:
+     bool hasCycle(ListNode *head) {
+        if (!head) return false;
+        ListNode *first = head;
+        ListNode *second = head;
+        while (second->next && second->next->next) {
+            first = first->next;
+            second = second->next->next; 
+            if (first == second) return true;
+        }
+        return false;
+    }
+    //------------------------------------------//
     bool hasCycle(ListNode *head) {
         // IMPORTANT: Please reset any member data you declared, as
         // the same Solution instance will be reused for each test case.
