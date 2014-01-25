9a58a133397f1077a2322527061962a43658f920
Xiaomou Wang <wxm19890422@gmail.com>
2014-01-25 10:56:06 +0800

    mid hard

--- surroundedRegions.cc
+++ surroundedRegions.cc
@@ -8,6 +8,7 @@ public:
         int col = board[0].size();
         vector<int> _row;
         vector<int> _col;
+        
         //search the eadge to find 'O' to start BFS
         for (int i = 0; i < row; i++) {
             if (board[i][0] == 'O') {
@@ -67,6 +68,68 @@ public:
             }
         }
         
+    }
+
+    //-------------------------------------------------------------//
+    void solve(vector<vector<char>> &board) {
+        int m = board.size();
+        if (!m) return;
+        int n = board[0].size();
+        queue<pair<int, int>> q;
+        
+        for (int i = 0; i < m; i++) {
+            if (board[i][0] == 'O') q.push(make_pair(i, 0));//bfs(board, i, 0);
+            if (board[i][n - 1] == 'O') q.push(make_pair(i, n - 1));//bfs(board, i, n - 1);
+        }
+        for (int j = 0; j < n; j++) {
+            if (board[0][j] == 'O') q.push(make_pair(0, j));//bfs(board, 0, j);
+            if (board[m - 1][j] == 'O') q.push(make_pair(m - 1, j));//bfs(board, m - 1, j);
+        }
+        
+        while (q.size()) {
+            int i1 = q.front().first;
+            int j1 = q.front().second;
+            if (i1 > 0 && board[i1 - 1][j1] == 'O')
+                q.push(make_pair(i1 - 1, j1));
+            if (i1 < m - 1 && board[i1 + 1][j1] == 'O')
+                q.push(make_pair(i1 + 1, j1));
+            if (j1 > 0 && board[i1][j1 - 1] == 'O')
+                q.push(make_pair(i1, j1 - 1));
+            if (j1 < n - 1 && board[i1][j1 + 1] == 'O')
+                q.push(make_pair(i1, j1 + 1));
+            board[i1][j1] = 'Y';
+            q.pop();
+        }
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'Y')
+                    board[i][j] = 'O';
+                else if (board[i][j] == 'O')
+                    board[i][j] = 'X';
+            }
+        }
+    }
+    
+    inline void bfs(vector<vector<char>> &board, int i, int j) {
+        int m = board.size();
+        int n = board[0].size();
+        queue<pair<int, int>> q;
+        q.push(make_pair(i, j));
+        while (q.size()) {
+            int i1 = q.front().first;
+            int j1 = q.front().second;
+            if (i1 > 0 && board[i1 - 1][j1] == 'O')
+                q.push(make_pair(i1 - 1, j1));
+            if (i1 < m - 1 && board[i1 + 1][j1] == 'O')
+                q.push(make_pair(i1 + 1, j1));
+            if (j1 > 0 && board[i1][j1 - 1] == 'O')
+                q.push(make_pair(i1, j1 - 1));
+            if (j1 < n - 1 && board[i1][j1 + 1] == 'O')
+                q.push(make_pair(i1, j1 + 1));
+            board[i1][j1] = 'Y';
+            q.pop();
+        }
     }
   
 };
