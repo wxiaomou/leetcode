b3866e1dda1d6646ae54d977bb200816f4a42481
Xiaomou Wang <wxm19890422@gmail.com>
2014-01-25 11:47:41 +0800

    ease mid

--- isPalindrome.cc
+++ isPalindrome.cc
@@ -1,5 +1,23 @@
 class Solution {
 public:
+    //w/o extral space
+    bool isPalindrome(string s) {
+        int n = s.length();
+        transform(s.begin(), s.end(), s.begin(), ::tolower);
+        if (!n) return true;
+        int i = 0;
+        int j = n - 1;
+        while (i <= j) {
+            while(!((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= '0' && s[i] <= '9'))&& i < n) i++;
+            while(!((s[j] >= 'a' && s[j] <= 'z') || (s[j] >= '0' && s[j] <= '9'))&& j >= 0) j--;
+            if (i > j) break;
+            if (s[i] != s[j]) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+    //----------------------------------//
     bool isPalindrome(string s) {
         // IMPORTANT: Please reset any member data you declared, as
         // the same Solution instance will be reused for each test case.
