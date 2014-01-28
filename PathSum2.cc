960a021df62b228f81f33e1bd4f6ca7a4a40e452
Xiaomou Wang <wxm19890422@gmail.com>
2014-01-28 08:15:33 +0800

    mid

--- PathSum2.cc
+++ PathSum2.cc
@@ -10,6 +10,23 @@
 class Solution {
     public:
     vector<vector<int> > pathSum(TreeNode *root, int sum) {
+        if (!root) return vector<vector<int>>();
+        if (!root->left && !root->right && root->val == sum) {
+            return vector<vector<int>> (1, vector<int>(1, sum));
+        }
+        vector<vector<int>> left;
+        vector<vector<int>> right;
+        vector<vector<int>> ret;
+        if (root->left) left = pathSum(root->left, sum - root->val); 
+        if (root->right) right = pathSum(root->right, sum - root->val);
+        for (int i = 0; i < left.size(); i++) left[i].insert(left[i].begin(), root->val);
+        for (int i = 0; i < right.size(); i++) right[i].insert(right[i].begin(), root->val);
+        if (left.size()) ret.insert(ret.end(), left.begin(), left.end());
+        if (right.size()) ret.insert(ret.end(), right.begin(), right.end());
+        return ret;
+    }
+    //-----------------------------------------------------------//    
+    vector<vector<int> > pathSum(TreeNode *root, int sum) {
         // IMPORTANT: Please reset any member data you declared, as
         // the same Solution instance will be reused for each test case.
         vector<vector<int> > ret;
