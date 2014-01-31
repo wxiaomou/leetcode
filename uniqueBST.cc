3c8be00eeb04e0208d278e466365f869e2880d2f
Xiaomou Wang <wxm19890422@gmail.com>
2014-01-31 19:42:37 +0800

    mid

--- uniqueBST.cc
+++ uniqueBST.cc
@@ -5,6 +5,33 @@ using namespace std;
 class Solution {
 public:
 	int numTrees(int n) {
+       if (!n) return 0;
+       int ref[n + 1];
+       for (int i = 0; i <= n; i++) ref[i] = -1;
+       recur(n, ref);
+       return ref[n];
+    }
+    
+    int recur(int n, int ref[]) {
+        if (n <= 0) return 0;
+        if (n == 1) {
+            ref[n] = 1;
+            return 1;
+        }
+        if (ref[n] != -1) return ref[n];
+        ref[n] = 0;
+        for (int i = 1; i <= n; i++) {
+            int l = recur(i - 1, ref);
+            int r = recur(n - i, ref);
+            if (l && r)
+                ref[n] += l * r;
+            else 
+                ref[n] += l ? l : r;
+        }
+        return ref[n];
+    }
+    //-----------------------------------------//
+	int numTrees(int n) {
 		 if (n < 1)
             return 0;
         if (n == 1)
