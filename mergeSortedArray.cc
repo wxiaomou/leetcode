2f2f6d58f97a0a3ed4dab5cea6bcef99162e2b33
Xiaomou Wang <wxm19890422@gmail.com>
2014-02-02 18:28:53 +0800

    mid

--- mergeSortedArray.cc
+++ mergeSortedArray.cc
@@ -5,6 +5,20 @@ using namespace std;
 class Solution {
 public:
 	void merge(int A[], int m, int B[], int n) {
+        int i = m - 1, j = n - 1, k = m + n - 1;
+        while (k >= 0) {
+            if (i >= 0 && j >= 0) {
+                A[k--] = A[i] > B[j] ? A[i--] : B[j--]; 
+            } else if (i >= 0) {
+                A[k--] = A[i--];
+            } else {
+                A[k--] = B[j--];
+            }
+        }
+        return;
+    }
+	//------------------------------------------//
+	void merge(int A[], int m, int B[], int n) {
 	 	int k = m + n - 1;
     	int i = m - 1;
 		int j = n - 1;
