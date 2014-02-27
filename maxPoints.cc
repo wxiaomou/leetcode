be08b3834a668296d90982124beabbdaa376564c
Xiaomou Wang <wxm19890422@gmail.com>
2014-02-26 23:22:02 -0800

    hard

--- maxPoints.cc
+++ maxPoints.cc
@@ -10,6 +10,37 @@
 class Solution {
 public:
     int maxPoints(vector<Point> &points) {
+        int n = points.size();
+        int ret = 0;
+        unordered_map<float, int> _map;
+        for (int i = 0; i < n; i++) {
+            _map.clear();
+            int same = 0, tmp = 1;
+            for (int j = i + 1; j < n; j++) {
+                double k = std::numeric_limits<double>::infinity();
+                if (points[j].x != points[i].x) {
+                    k = (float) (points[j].y - points[i].y) / (points[j].x - points[i].x); // if x diff then can compute the slop O.W. k shoud be infinity;
+                } else if (points[j].y == points[i].y) {
+                    same++;
+                    continue;
+                }
+                if (_map.count(k) == 0) {
+                    tmp = max(2, tmp);
+                    _map[k] = 2;
+                } else {
+                    tmp = max(++_map[k], tmp);
+                }
+            }
+            ret = max(ret, tmp + same);
+        }
+        return ret;
+    }
+/*for j start w/ i + 1, each time we treat ipoints[i] as the left point of the line, because slop is consist nomatter points[i]->points[j] or reverse it's not a 向量
+  所以只要线上的一个点出现过这条线就已经计算过了，j就没必要在从0开始了 (points[j].y - points[i].y) / (points[j].x - points[i].x) ＝＝ (points[i].y - points[j].y) / (points[i].x - points[j].x)
+  a, b, c, d 当a出现过时此条线已经计算过了， 到b的时候就不用考虑a了*/
+
+    //---------------------------------------------------------------//
+    int maxPoints(vector<Point> &points) {
         if (points.size() <= 1 || isAllSame(points)) return points.size();
         int _max = 2;
         for (int i = 0; i < points.size(); i++) {
