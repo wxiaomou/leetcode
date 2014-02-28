4210df01b83ec77c802e093c94f45128a692c0eb
Xiaomou Wang <wxm19890422@gmail.com>
2014-02-27 23:08:14 -0800

    hard

--- candy.cc
+++ candy.cc
@@ -4,6 +4,24 @@ using namespace std;
 class Solution {
 public:
     int candy(vector<int> &ratings) {
+        int n = ratings.size();
+        vector<int> candies(n, 1);
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1])
+                candies[i] = candies[i - 1] + 1;
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                if (candies[i] == 1 || candies[i] <= candies[i + 1] + 1)
+                    candies[i] = candies[i + 1] + 1;
+            }
+        }
+        int ret = 0;
+        for (int i = 0; i < n; i++) ret += candies[i];
+        return ret;
+    }
+    //-----------------------------------//
+    int candy(vector<int> &ratings) {
         // Note: The Solution object is instantiated only once and is reused by each test case.
         if (!ratings.size()) return 0;
         int ret = ratings.size();
