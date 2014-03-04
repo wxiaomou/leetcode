045e816ac469af51e7011a54183887fc252c2d5a
Xiaomou Wang <wxm19890422@gmail.com>
2014-03-03 22:10:48 -0800

    hard

--- regex.cc
+++ regex.cc
@@ -15,6 +15,49 @@ public:
             return isMatch(s, p + 2);
         }
     }
+
+    //-----------------------------//
+    bool isMatch(const char *s, const char *p) {
+        if (*p == 0) return *s == 0;
+        int m = strlen(s), n = strlen(p);
+        vector<int> tmp(n, -1);
+        vector<vector<int>> dp(m, tmp);
+        return _dp(dp, s, p, 0, 0) == 1;
+    }
+    
+    int _dp(vector<vector<int>> &dp, const char *s, const char *p, int i, int j) {
+        int m = strlen(s), n = strlen(p);
+        if (j == n) return i == m;
+        if (i == m) {
+            if (*(p + j + 1) == '*')
+                return _dp(dp, s, p, i, j + 2);
+            else 
+                return 0;
+        }
+        
+        if (dp[i][j] != -1) return dp[i][j];
+        
+        if (j + 1 == n || p[j + 1] != '*') {
+            if (s[i] == p[j] || (p[j] == '.' && i < m)) {
+                dp[i][j] = _dp(dp, s, p, i + 1, j + 1);
+            } else { 
+                dp[i][j] = 0;
+            }
+            return dp[i][j];
+        } else {
+            while(s[i] == p[j] || (p[j] == '.' && i < m)) {
+                if (_dp(dp, s, p, i, j + 2)) {
+                    dp[i][j] = 1;
+                    return 1;
+                }
+                i++;
+            }
+            int tmp = _dp(dp, s, p, i, j + 2);
+            if (i < m) 
+                dp[i][j] = tmp;
+            return tmp;
+        }
+    }
     /*bool isMatch(const char *s, const char *p) {
         if (*p == 0) return *s == 0;
         if (*s == 0) {
