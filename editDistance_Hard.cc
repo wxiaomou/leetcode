4011be2b1c880937bb5e4d79ed10d2e3b9538230
Xiaomou Wang <wxm19890422@gmail.com>
2014-03-10 20:09:07 -0700

    hard

--- editDistance_Hard.cc
+++ editDistance_Hard.cc
@@ -5,6 +5,26 @@ using namespace std;
 class Solution {
 public: 
 	int minDistance(string word1, string word2) {
+        int m = word1.length();
+        int n = word2.length();
+        if (!m || !n) return m ? m : n;
+        vector<int> tmp(n + 1, INT_MAX);
+        vector<vector<int>> dp(m + 1, tmp);
+        for (int j = 0; j <= n; j++) dp[0][j] = j; // j means, if start from here, need to delete j elements in word2 first, so the cost is j here
+        for (int i = 1; i <= m; i++) dp[i][0] = i;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = min( dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1]) ) + 1;// i-1 & j-1 is replacement, i - 1 is deleting in word1, j - 1 is inserting in word1, only consern change in one word.
+                }
+            }
+        }
+        return dp[m][n];
+    }
+	//---------------------------------------//
+	int minDistance(string word1, string word2) {
 		int m = word1.length();
 		int n = word2.length();
 		int A[m + 1][n + 1];
