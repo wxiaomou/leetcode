9e64ac46537defb8437a74acfab9ac5d99bf1fa1
Xiaomou Wang <wxm19890422@gmail.com>
2014-03-11 23:51:28 -0700

    hard

--- longestConsecutive.cc
+++ longestConsecutive.cc
@@ -1,5 +1,32 @@
 class Solution {
 public:
+nt longestConsecutive(vector<int> &num) {
+        int n = num.size();
+        unordered_map<int, int> hash;
+        if (!n) return 0;
+        int first = 0, last = 0;
+        for (int i = 0; i < n; i++) {
+            if (hash.count(num[i]) > 0) continue;
+            
+            hash[num[i]] = num[i];
+            
+            int beg = num[i], end = num[i];
+            if (hash.count(num[i] - 1) > 0) // because for each num not in the hash we will look for num - 1 and num + 1, so for every seq nomatter the order it may have, we can reach the begin and end, e.g. 9, 4, 3, 8, 5, 6
+                beg = hash[num[i] - 1];
+            if (hash.count(num[i] + 1) > 0)
+                end = hash[num[i] + 1];
+            
+            hash[beg] = end; // because next we access the num beg, the only way is from number (num - 1), because num and num + 1 are already in the hash, and they will immediately continue; so here hash[brg] = end;
+            hash[end] = beg;
+            if (end - beg + 1 > last - first + 1) {
+                first = beg;
+                last = end;
+            }
+        }
+        return last - first + 1;
+    }
+    //------------------------------------//
+
     int longestConsecutive(vector<int> &num) {
         // IMPORTANT: Please reset any member data you declared, as
         // the same Solution instance will be reused for each test case.
