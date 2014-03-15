c89dc0df46b58372e11c22254ff17d8aaac5cd2a
Xiaomou Wang <wxm19890422@gmail.com>
2014-03-15 13:49:21 -0700

    hard

--- wordBreak2.cc
+++ wordBreak2.cc
@@ -48,7 +48,7 @@ public:
             }
         }
         if (!ref[0][n]) return vector<string>();
-        string tmp = "";
+        string tmp = "";gi
         dfs(s, 0, tmp, dict, ref, ret);
         return ret;
     }
@@ -68,6 +68,52 @@ public:
                     tmp += s.substr(i, len);
                     tmp += " ";
                     dfs(s, i + len, tmp, dict, ref, ret);
+                    tmp = tmp.substr(0, len_before);
+                }
+            }
+        }
+        return;
+    }
+
+    //-----------------------------------------------//
+     vector<string> wordBreak(string s, unordered_set<string> &dict) {
+        int n = s.length();
+        if (!n) return vector<string>();
+        vector<bool> tmp(n, false);
+        vector<vector<bool>> dp(n, tmp);
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                if (dict.count(s.substr(i, j - i + 1)) > 0) {
+                    dp[i][j] = true;
+                    continue;
+                }
+                for (int k = i; k < j; k++) {
+                    if (dp[i][k] && dp[k + 1][j]) {
+                        dp[i][j] = true;
+                        break;
+                    }
+                }
+            }
+        }
+        if (!dp[0][n - 1]) return vector<string>();
+        vector<string> ret;
+        dfs(s, 0, ret, "", dp, dict);
+        return ret;
+    }
+    
+    void dfs(string &s, int start, vector<string> &ret, string tmp, vector<vector<bool>> &dp, unordered_set<string> &dict) {
+        int n = s.length();
+        if (start == n) {
+            ret.push_back(tmp.substr(0, tmp.length() - 1));   
+            return;
+        }
+        for (int len = 1; start + len <= n; len++) {
+            if (dict.count(s.substr(start, len)) > 0) {
+                if (dp[start][start + len - 1]) {
+                    int len_before = tmp.length();
+                    tmp += s.substr(start, len);
+                    tmp += " ";
+                    dfs(s, start + len, ret, tmp, dp, dict);
                     tmp = tmp.substr(0, len_before);
                 }
             }
