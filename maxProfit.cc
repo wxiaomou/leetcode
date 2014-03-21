6480d6ce962fd91f3e2aa50f8157b5edaa819f23
Xiaomou Wang <wxm19890422@gmail.com>
2014-03-20 20:25:04 -0700

    ease

--- maxProfit.cc
+++ maxProfit.cc
@@ -1,6 +1,19 @@
 class Solution {
     public:
     int maxProfit(vector<int> &prices) {
+        int n = prices.size();
+        if (n < 2) return 0;
+        int ret = 0;
+        int _min = prices[0];
+        for (int i = 1; i < n; i++) {
+            _min = min(prices[i], _min);
+            ret = max(ret, prices[i] - _min);
+        }
+        return ret;
+    }
+
+    //----------------------------------------//
+    int maxProfit(vector<int> &prices) {
         // the same Solution instance will be reused for each test case.
         if (!prices.size()) return 0;
         int _min = prices[0];
@@ -14,4 +27,5 @@ class Solution {
         }
         return ret;
     }
+
 };
