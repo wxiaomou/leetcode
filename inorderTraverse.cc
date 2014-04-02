6a6bf7e61ccfcf2c7c94e0fc8ce446f5ec848895
Xiaomou Wang <wxm19890422@gmail.com>
2014-04-01 22:54:45 -0700

    mid hard

--- inorderTraverse.cc
+++ inorderTraverse.cc
@@ -9,6 +9,36 @@
  */
 class Solution {
 public:
+
+ vector<int> inorderTraversal(TreeNode *root) {
+        if (!root) return vector<int>();
+        vector<int> ret;
+        stack<TreeNode *> s;
+        s.push(root);
+        TreeNode *cur = root->left;
+        while (cur) {
+            s.push(cur);
+            cur = cur->left;
+        }
+        
+        while (s.size()) {
+            TreeNode *cur = s.top();
+            ret.push_back(cur->val);
+            s.pop();
+            if (cur->right) {
+                s.push(cur->right);
+                cur = cur->right;
+                cur = cur->left;
+                while (cur) {
+                    s.push(cur);
+                    cur = cur->left;
+                }
+            }
+        }
+        return ret;
+    }
+
+    //-----------------------------//
     vector<int> inorderTraversal(TreeNode *root) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
