f5d95958d91c429703dd4387a4225d5dee2d6341
wxiaomou <wxm19890422@gmail.com>
2016-08-14 13:56:04 -0700

    Create wildcard.java
    dp solution of wildcard

---
+++ wildcard.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public boolean isMatch(String s, String p) {
+       int m = s.length();
+       int n = p.length();
+       if (n == 0) {
+           return m == 0;
+       }
+       boolean[][] dp = new boolean[m + 1][n + 1];
+       for (int i = 0; i <= m; i++) {
+           for (int j = 0; j <= n; j++) {
+               dp[i][j] = false;
+           }
+       }
+       dp[0][0] = true;
+       for (int j = 1; j <= n; j++) {
+            dp[0][j] = dp[0][j - 1] && (p.charAt(j - 1) == '*');
+       }
+       for (int i = 1; i <= m; i++) {
+           for (int j = 1; j <= n; j++) {
+               if (p.charAt(j - 1) == '*') {
+                   dp[i][j] = dp[i][j - 1] || dp[i - 1][j]; // * match s[i - 1] (dp[i - 1][j]) or match nothing (dp[i][j - 1])
+               } else {
+                   dp[i][j] = dp[i - 1][j - 1] && (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '?');
+               }
+           }
+       }
+       return dp[m][n];
+    }
+}
